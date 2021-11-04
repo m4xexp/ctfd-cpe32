a5301fb870f0daa78642cf20324dfd13dc857a88
foREVerxe <fewstriker56@hotmail.com>
2021-11-04 16:21:25 +0700

    2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           pom mute pai gon.
+           pom mute pai gon2.
         </p>
         <a
           className="App-link"
