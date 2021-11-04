168d129c3e23d527c5b201c86740d7384b5a5bb7
foREVerxe <fewstriker56@hotmail.com>
2021-11-04 17:29:51 +0700

    add registry

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           pom mute pai gon3.
+           Test Project.
         </p>
         <a
           className="App-link"
