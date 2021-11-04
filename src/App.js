a71dc2f63e06ceb73a5b2a6b9166a1955a1244c5
foREVerxe <fewstriker56@hotmail.com>
2021-11-04 16:19:29 +0700

    change react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> pom mute pai gon.
+           pom mute pai gon.
         </p>
         <a
           className="App-link"
