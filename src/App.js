7930f5c96ac7591b11e0cc160feda24a483edb53
foREVerxe <fewstriker56@hotmail.com>
2021-11-07 16:34:39 +0700

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Test Project.
+           Test Project kub.
         </p>
         <a
           className="App-link"
